4698e04091edcbbc04cd66fe329bea476321f046
Sai Dinesh <60782915+saidinesh898@users.noreply.github.com>
2022-03-15 12:22:55 +0530

    Add files via upload

---
+++ initialScriptForCapRover.sh
@@ -0,0 +1,20 @@
+sudo apt update;
+sudo apt upgrade -y;
+sudo ufw allow 22;
+ufw allow 80;
+ufw allow 443;
+ufw allow 3000;
+ufw allow 996;
+ufw allow 7946;
+ufw allow 4789;
+ufw allow 2377; 
+sudo iptables -F;
+echo "y" | sudo ufw enable;
+sudo systemctl stop iptables;
+sudo systemctl disable iptables;
+
+echo "y" | apt install npm 
+echo "y" | apt install docker.io
+
+docker run -p 80:80 -p 443:443 -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
+
