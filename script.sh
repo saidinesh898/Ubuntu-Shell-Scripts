8085b523b3e61b0e1d75731ef1f8d0e6b97ccde6
Sai Dinesh <60782915+saidinesh898@users.noreply.github.com>
2022-03-15 11:17:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 sudo apt update;
 sudo apt upgrade -y;
 sudo ufw allow 22;
+ufw allow 80,443,3000,996,7946,4789,2377/tcp; ufw allow 7946,4789,2377/udp;
 echo "y" | sudo ufw enable;
 sudo iptables -F;
 systemctl stop iptables;
